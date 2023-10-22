6e9e05b39074fc6465bbf8a3da8197d121f9410c
Avneet Singh <avneet.ch02@gmail.com>
2023-10-22 22:01:07 +0530

    feature
    Added priority button

--- app/page.js
+++ app/page.js
@@ -5,25 +5,49 @@ const Page = () => {
   const [title, setTitle] = useState('');
   const [desc, setDesc] = useState('');
   const [mainTask, setMainTask] = useState([]);
+  const [priority, setPriority] = useState('Low');
 
   const submitHandler = (e) => {
     e.preventDefault();
-    setMainTask([...mainTask,{title,desc }])
+    setMainTask([...mainTask,{title,desc ,priority}])
     setTitle('');
     setDesc('');
+    setPriority('Low');
     console.log(mainTask)
   };
-  let renderTask=<h2>No task available to show</h2>
-  if(mainTask.length>0)
-  {
-    renderTask=mainTask.map((t,i)=>{
-      return 
-      <li><div className='flex justify-between mb-5 '>
-        <h5 className='text-2xl font-bold'>{t.title}</h5>
-        <h5 className='text-xl font-semi-bold'>{t.desc}</h5>
-      </div></li>
-  })
+  const deletehandler=(i) =>{
+  let copyTask=[...mainTask]
+  copyTask.splice(i,1)
+  setMainTask(copyTask)
   }
+  const completehandler=(i)=>{
+    let copyTask2=[...mainTask]
+    copyTask2.splice(i,1)
+    setMainTask(copyTask2)
+  }
+  let renderTask=<h2>No task available to show</h2>
+  if(mainTask.length>0){
+
+  renderTask=mainTask.map((t,i)=>{
+    return <li key={i} className='flex items-center justify-between'>
+      <div className='flex justify-between mb-5 w-2/3'>
+      <h5 className='text-2xl font-semibold'>{t.title}</h5>
+      <h6 className='text-2xl font-semibold'>{t.desc}</h6>
+      <p className='text-2xl font-semibold'>Priority: {t.priority}</p> 
+    </div>
+    <button 
+    onClick={()=>{
+      deletehandler(i)
+    }}
+    className='bg-red-400 text-white px-4 py-2 rounded font-bold'>Delete</button>
+    <button
+    onClick={()=>{
+      completehandler(i)
+    }}
+     className='bg-green-400 text-white px-4 py-3 rounded font-bold '>Completed</button>
+    </li>
+  })
+}
   
 
   return (
@@ -50,6 +74,16 @@ const Page = () => {
             setDesc(e.target.value);
           }}
         />
+        <select
+          value={priority}
+          onChange={(e) => setPriority(e.target.value)}
+          className="text-2xl  bg-blue-300 border border-black m-8 px-6 py-4 rounded font-bold "
+        >
+          <option value="Low">Low</option>
+          <option value="Medium">Medium</option>
+          <option value="High">High</option>
+        </select>
+        
         <button className='bg-black text-white px-4 py-2 text-2xl font-bold rounded m-5'>
           Add task
         </button>
